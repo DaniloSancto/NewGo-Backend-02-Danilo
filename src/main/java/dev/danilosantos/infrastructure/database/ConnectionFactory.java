e2543e0d52a68fded45a635636787fa8c01ce39e
Danilo Santos <danilo.santos0404@hotmail.com>
2023-09-06 14:51:26 -0300

    refactor: import não utilizado e variaveis para final

--- src/main/java/dev/danilosantos/infrastructure/database/ConnectionFactory.java
+++ src/main/java/dev/danilosantos/infrastructure/database/ConnectionFactory.java
@@ -2,12 +2,11 @@ package dev.danilosantos.infrastructure.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private String url = "jdbc:postgresql://localhost:5432/atividadenewgo";
-    private String user = "postgres";
-    private String password = "postgres";
+    private final String url = "jdbc:postgresql://localhost:5432/atividadenewgo";
+    private final String user = "postgres";
+    private final String password = "postgres";
 
     public Connection getConnection() {
         try {
