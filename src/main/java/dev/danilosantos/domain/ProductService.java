6ef3767aaa90c9c09bc166e2e0ad6e71db3e04e8
Danilo Santos <danilo.santos0404@hotmail.com>
2023-09-12 10:33:58 -0300

    feat: RN004
    Não permitir cadastrar produtos com preço, quantidade ou estoque_min negativo.

--- src/main/java/dev/danilosantos/domain/ProductService.java
+++ src/main/java/dev/danilosantos/domain/ProductService.java
@@ -28,7 +28,20 @@ public class ProductService {
             throw new BaseException("ean13 do produto ja cadastrado");
         }
 
+        verifyNegativeValues(product.getPrice(), product.getQuantity(), product.getMinStorage());
         productDao.insert(product);
+    }
+
+    private void verifyNegativeValues(Double price, Double quantity, Double minStorage) {
+        if (price< 0) {
+            throw new BaseException("preco nao pode ser um valor negativo");
+        }
+        else if (quantity < 0) {
+            throw new BaseException("quantidade nao pode ser um valor negativo");
+        }
+        else if (minStorage < 0) {
+            throw new BaseException("estoque_minimo nao pode ser um valor negativo");
+        }
     }
 
     private Product dtoToEntity(ProductInsertDto dto) {
