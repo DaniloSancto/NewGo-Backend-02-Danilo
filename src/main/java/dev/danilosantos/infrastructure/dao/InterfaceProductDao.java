5ef5e680405855e47f778fabfa7768a6b6ef3188
Danilo Santos <danilo.santos0404@hotmail.com>
2023-09-13 14:02:41 -0300

    feat: metodos findByHash e updateByHash na Interface

--- src/main/java/dev/danilosantos/infrastructure/dao/InterfaceProductDao.java
+++ src/main/java/dev/danilosantos/infrastructure/dao/InterfaceProductDao.java
@@ -9,11 +9,15 @@ public interface InterfaceProductDao {
 
     Product insert(Product product);
 
+    boolean updateByHash(UUID hash, Product product);
+
     List<Product> findAll();
 
     Product findByName(String param);
 
     Product findByEan13(String param);
+
+    Product findByHash(UUID hash);
 
     UUID findHash(UUID param);
 
