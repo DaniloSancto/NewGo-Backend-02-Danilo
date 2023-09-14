af201322927f5e0dc0bddad114f663554429ef2f
Danilo Santos <danilo.santos0404@hotmail.com>
2023-09-14 09:58:51 -0300

    refactor: nome das variáveis de conexão em letra maiuscula

--- src/main/java/dev/danilosantos/infrastructure/database/ConnectionFactory.java
+++ src/main/java/dev/danilosantos/infrastructure/database/ConnectionFactory.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-    private final String url = "jdbc:postgresql://localhost:5432/atividadenewgo";
-    private final String user = "postgres";
-    private final String password = "postgres";
+    private final String URL = "jdbc:postgresql://localhost:5432/atividadenewgo";
+    private final String USER = "postgres";
+    private final String PASSWORD = "postgres";
 
     public Connection getConnection() {
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(url, user, password);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
