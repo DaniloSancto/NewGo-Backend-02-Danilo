b8c443336d21966321ace424c22988a94866eae4
Danilo Santos <63885658+DaniloSancto@users.noreply.github.com>
2023-09-26 15:06:58 -0300

    feat: adicionado um enum de strings ExceptionsMessages

---
+++ src/main/java/dev/danilosantos/domain/strings/ExceptionMessages.java
@@ -0,0 +1,24 @@
+package dev.danilosantos.domain.strings;
+
+public enum ExceptionMessages {
+    PRODUCT_NOT_FIND("Produto não encontrado"),
+    NAME_CANNOT_BE_NULL_OR_EMPTY("Nome do produto não pode ser nulo ou vazio"),
+    NAME_ALREADY_REGISTERED("Nome do produto já cadastrado"),
+    EAN13_ALREADY_REGISTERED("EAN13 do produto já cadastrado"),
+    PRODUCT_INACTIVE("produto inativo"),
+    INACTIVE_PRODUCT_CANNOT_BE_UPDATED("Produto inativo não pode ser atualizado"),
+    PRICE_CANNOT_BE_NEGATIVE("Preço não pode ser um valor negativo"),
+    QUANTITY_CANNOT_BE_NEGATIVE("Quantidade não pode ser um valor negativo"),
+    STORAGE_CANNOT_BE_NEGATIVE("EstoqueMin não pode ser um valor negativo"),
+    ;
+
+    private final String message;
+
+    ExceptionMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
